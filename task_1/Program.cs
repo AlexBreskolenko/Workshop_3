3c260891d20c8ae2a1070f6f863aecc0c08fa412
Alex <abreskolenko@yandex.ru>
2023-02-18 00:15:53 +0300

    Доделать 1 задачу

--- task_1/Program.cs
+++ task_1/Program.cs
@@ -9,7 +9,7 @@ Console.Clear();
 //Метод создание массива нужной длинны
 int[] CreateArray(int lengthArray)
 {
-    return new int [lengthArray];
+    return new int[lengthArray];
 }
 //Метод заполнения массива 
 void FillArray(int[] arr)
@@ -27,7 +27,32 @@ void PrintArray(int[] arr)
         Console.Write(arr[i] + " , ");
     }
 }
+//Метод премешивания массива
+void MixArray(int[] arr)
+{
+    for(int i = arr.Length - 1; i >= 1 ; i--)
+    {
+        int j = i + 1;
+        int temporary = arr[j];
+        arr[j] = arr[i];
+        arr[i] = temporary;
+    }
+}
 
 int[] array = CreateArray(10);
 FillArray(array);
 PrintArray(array);
+MixArray(array);
+Console.WriteLine();
+PrintArray(array);
+
+/*
+for (int i = data.Length - 1; i >= 1; i--)
+{
+   int j = random.Next(i + 1);
+   // обменять значения data[j] и data[i]
+   var temp = data[j];
+   data[j] = data[i];
+   data[i] = temp;
+}
+*/
