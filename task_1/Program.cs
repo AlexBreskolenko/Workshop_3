1ad48222bc812d843a416971c0a4fd94bc59ca17
Alex <abreskolenko@yandex.ru>
2023-02-17 13:47:18 +0300

    Workshop 3

---
+++ task_1/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+Реализовать перемешивание массива
+
+1 1 2 3 4 5 6 7 8 
+*/
+
+Console.Clear();
+
+int[] array = {5, 9, 4, 2, 6, 10};
+
+for(int i = 0; i < array.Length - 1; i++)
+{
+        array[i] = array[i + 1];
+}
+
+for (int i = 0; i < array.Length;i++)
+{
+    Console.Write(array[i] + " ");
+}
---
+++ task_2/Program.cs
@@ -0,0 +1,45 @@
+﻿/*
+дан массив: наполнени числами от 1 до N
+Задача: сколько раз какое число встречается
+
+1 3 1 2 1 2 4 5 1 2 5 4
+
+1 - 4р
+2 - 3р
+3 - 1р
+4 - 2р
+5 - 2р
+
+*/
+int[] arr = new arr[15];
+int num_1, num_2, num_3,num_4, num_5, num_6;
+int[] array = {5, 3, 1, 2, 4, 6};
+
+for (int i = 0; i < array.Length; i++)
+{
+    if(array[i] == 1)
+    {
+        num_1++;
+    }
+    else if(array[i] == 2)
+    {
+        num_2++;
+    }
+    else if(array[i] == 3)
+    {
+        num_3++;
+    }
+    else if(array[i] == 4)
+    {
+        num_4++;
+    }
+    else if(array[i] == 5)
+    {
+        num_5++;
+    }
+    else if(array[i] == 6)
+    {
+        num_6++;
+    }
+    Console.WriteLine();
+}
