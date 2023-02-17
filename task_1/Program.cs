6c5e6e56d32e8ba18ab503c72806faa1e09a0166
Alex <abreskolenko@yandex.ru>
2023-02-17 22:46:30 +0300

    Create task 1

--- task_1/Program.cs
+++ task_1/Program.cs
@@ -6,14 +6,28 @@
 
 Console.Clear();
 
-int[] array = {5, 9, 4, 2, 6, 10};
-
-for(int i = 0; i < array.Length - 1; i++)
+//Метод создание массива нужной длинны
+int[] CreateArray(int lengthArray)
 {
-        array[i] = array[i + 1];
+    return new int [lengthArray];
+}
+//Метод заполнения массива 
+void FillArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = i;
+    }
+}
+//Метод вывода массива
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(arr[i] + " ,");
+    }
 }
 
-for (int i = 0; i < array.Length;i++)
-{
-    Console.Write(array[i] + " ");
-}
+int[] array = CreateArray(10);
+FillArray(array);
+PrintArray(array);
--- task_2/Program.cs
+++ task_2/Program.cs
@@ -11,35 +11,4 @@
 5 - 2р
 
 */
-int[] arr = new arr[15];
-int num_1, num_2, num_3,num_4, num_5, num_6;
-int[] array = {5, 3, 1, 2, 4, 6};
 
-for (int i = 0; i < array.Length; i++)
-{
-    if(array[i] == 1)
-    {
-        num_1++;
-    }
-    else if(array[i] == 2)
-    {
-        num_2++;
-    }
-    else if(array[i] == 3)
-    {
-        num_3++;
-    }
-    else if(array[i] == 4)
-    {
-        num_4++;
-    }
-    else if(array[i] == 5)
-    {
-        num_5++;
-    }
-    else if(array[i] == 6)
-    {
-        num_6++;
-    }
-    Console.WriteLine();
-}
